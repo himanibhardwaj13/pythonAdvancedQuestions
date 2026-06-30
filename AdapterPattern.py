ac40043fa5c701573191da8fa4cf7d565559bf1c
Himani Bhardwaj <hrabhardwaj@gmail.com>
2026-06-30 18:03:19 +0530

    Update AdapterPattern.py

--- AdapterPattern.py
+++ AdapterPattern.py
@@ -1,1 +1,22 @@
+from abc import ABC, abstractmethod
+# Adapter Pattern: Creating Interface
+class OldHeater:
+    def heat(self):
+        print("Old heatre is heating")
 
+class NewHeaterInterface(ABC):
+    @abstractmethod
+    def run(self):
+        pass
+
+class NewHeaterFactory(NewHeaterInterface):
+    def __init__(self, old_heater: OldHeater):
+        self.old_heater = old_heater
+    
+    def run(self):
+        self.old_heater.heat()
+        
+old_heater = OldHeater()
+new_heater = NewHeaterFactory(old_heater)
+new_heater.run()
+        
