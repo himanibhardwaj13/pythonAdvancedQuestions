27e198f72492e6425a0bdb2ffccc2ad788566a31
Himani Bhardwaj <hrabhardwaj@gmail.com>
2026-06-30 17:42:15 +0530

    Update StrategyPattern.py

--- StrategyPattern.py
+++ StrategyPattern.py
@@ -1,1 +1,22 @@
-
+from abc import ABC, abstractmethod
+# Strategy Pattern: Swap the algorithms interchangable
+class EngeryStrategy:
+    def start(self):
+        pass
+class NormalMode(EngeryStrategy):
+    def start(self):
+        print("Normal Mode is all set.")
+        
+class EcoMode(EngeryStrategy):
+    def start(self):
+        print("ECO Mode is all set.")
+        
+class ThermoStat:
+    def __init__(self, strategy: EngeryStrategy):
+        self.strategy = strategy
+        
+    def run(self):
+        self.strategy.start()
+        
+thermo = ThermoStat(EcoMode())
+thermo.run()
