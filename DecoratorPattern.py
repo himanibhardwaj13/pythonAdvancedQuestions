d5304be774d4e46240b4ccba99e2801788a849aa
Himani Bhardwaj <hrabhardwaj@gmail.com>
2026-06-30 18:49:38 +0530

    Update DecoratorPattern.py

--- DecoratorPattern.py
+++ DecoratorPattern.py
@@ -1,1 +1,11 @@
-
+def log_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"Log: Executing {func.__name__}")
+        return func(*args, **kwargs)
+    return wrapper
+    
+@log_decorator
+def secure_door(action):
+    print(f"Door action: {action}")
+    
+secure_door("LOCK")
