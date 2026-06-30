b7fbf31c0c970101a13bac78711b165380ea2d8e
Himani Bhardwaj <hrabhardwaj@gmail.com>
2026-06-30 18:37:16 +0530

    Update CommandPattern.py

--- CommandPattern.py
+++ CommandPattern.py
@@ -1,1 +1,19 @@
+from abc import ABC, abstractmethod
+# Command Pattern: Encapsulate user actions
+class Command:
+    def __init__(self, action):
+        self.action=action
+    def execute(self):
+        self.action()
 
+def turn_light_on():
+    print("Light is ON")
+    
+def turn_light_off():
+    print("Light is OFF")
+    
+on_cmd = Command(turn_light_on)
+off_cmd = Command(turn_light_off)
+on_cmd.execute()
+off_cmd.execute()
+        
